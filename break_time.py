f78501f6633aa41841f2926aefb115299ccd12e7
Suhail Ahamed <suhail511@gmail.com>
2017-11-09 11:05:31 +0400

    open brwoser with song

--- break_time.py
+++ break_time.py
@@ -1,1 +1,5 @@
-print ("Hi, this program should give breaks if you are sitting on your PC for long")
+import webbrowser
+
+print ("Hi, this program should give breaks if you are sitting on your PC for longe")
+
+webbrowser.open("https://www.youtube.com/watch?v=ozv4q2ov3Mk")
