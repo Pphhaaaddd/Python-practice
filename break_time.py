4ac62052d9abe4a980868cfe8b7382978e6b9a15
Suhail Ahamed <suhail511@gmail.com>
2017-11-09 11:08:31 +0400

    wait before starting (10s)

--- break_time.py
+++ break_time.py
@@ -1,5 +1,7 @@
 import webbrowser
+import time
 
-print ("Hi, this program should give breaks if you are sitting on your PC for longe")
+print ("Hi, this program should give breaks if you are sitting on your PC for long")
 
+time.sleep(10)
 webbrowser.open("https://www.youtube.com/watch?v=ozv4q2ov3Mk")
