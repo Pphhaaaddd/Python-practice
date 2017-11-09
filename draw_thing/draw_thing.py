221ce3f07740764eee8dedd215138d40956bc688
Suhail Ahamed <suhail511@gmail.com>
2017-11-09 13:01:08 +0400

    draw circle using squares

--- draw_thing/draw_thing.py
+++ draw_thing/draw_thing.py
@@ -1,14 +1,9 @@
 import turtle
 
-def draw_square():
-    brad = turtle.Turtle()
-    brad.shape("turtle")
-    brad.color("green")
-    brad.speed(10)
-
+def draw_square(some_turtle):
     for i in range(4):
-        brad.forward(50)
-        brad.right(90)
+        some_turtle.forward(50)
+        some_turtle.right(90)
 
 def draw_circle():
     angie = turtle.Turtle()
@@ -20,18 +15,31 @@ def draw_circle():
 
 def draw_triangle():
     david = turtle.Turtle()
-    david.color("red")
+    david.color("yellow")
     david.speed(10)
 
     for i in range(3):
         david.forward(50)
         david.right(120)
 
+def draw_circleFromSquare(some_turtle):
+    for i in range(30):
+        draw_square(some_turtle)
+        some_turtle.left(10)
+
+
 # make a window to display the tutles
 window = turtle.Screen()
-window.bgcolor("blue")
-draw_square()
-draw_circle()
-draw_triangle()
+window.bgcolor("black")
+
+brad = turtle.Turtle()
+brad.shape("turtle")
+brad.color("yellow")
+brad.speed(15)
+
+#draw_square(brad)
+#draw_circle()
+#draw_triangle()
+draw_circleFromSquare(brad)
 
 window.exitonclick()
