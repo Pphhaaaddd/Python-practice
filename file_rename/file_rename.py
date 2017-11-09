aba583da67230296c8e3aa6fdd8ba6707b0b79e7
Suhail Ahamed <suhail511@gmail.com>
2017-11-09 11:56:02 +0400

    rename the files removing numbers

--- file_rename/file_rename.py
+++ file_rename/file_rename.py
@@ -6,7 +6,12 @@ def rename_files():
     # get file names from folder
     file_list = os.listdir(r"/home/phad/version-control/python-practice/file_rename/prank")
     print(file_list)
-    # rename the files removing the numbers
 
+    # rename the files removing the numbers
+    saved_path = os.getcwd()
+    print("Current working directory is ", saved_path)
+    #os.chdir("/home/phad/version-control/python-practice/file_rename/prank")
+    for file_name in file_list:
+        os.rename(file_name,file_name.translate(None, "0123456789"))
 
 rename_files()
