4708702cff9028947f23326f2132950bc2f1da60
Suhail Ahamed <suhail511@gmail.com>
2017-11-09 12:37:53 +0400

    draw circle as well

--- draw_thing/draw_thing.py
+++ draw_thing/draw_thing.py
@@ -4,17 +4,25 @@ def draw_square():
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("green")
-    brad.speed(1)
+    brad.speed(10)
 
     for i in range(4):
         brad.forward(50)
         brad.right(90)
 
+def draw_circle():
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("black")
+    angie.speed(10)
+
+    angie.circle(100)
+
 # make a window to display the tutles
 window = turtle.Screen()
 window.bgcolor("blue")
 draw_square()
-#draw_circle()
+draw_circle()
 #draw_triangle()
 
 window.exitonclick()
