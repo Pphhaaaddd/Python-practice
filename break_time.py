897fab5b854b7e3255e8f80cbf617b10f29cc491
Suhail Ahamed <suhail511@gmail.com>
2017-11-09 11:10:23 +0400

    loop it 3 times

--- break_time.py
+++ break_time.py
@@ -3,5 +3,6 @@ import time
 
 print ("Hi, this program should give breaks if you are sitting on your PC for long")
 
-time.sleep(10)
-webbrowser.open("https://www.youtube.com/watch?v=ozv4q2ov3Mk")
+for i in range(3):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=ozv4q2ov3Mk")
