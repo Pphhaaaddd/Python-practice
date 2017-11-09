f0c0982ebe453b41bdbc8a9f4184100e8a3d3895
Suhail Ahamed <suhail511@gmail.com>
2017-11-09 11:47:33 +0400

    save the file names and print them

--- file_rename/file_rename.py
+++ file_rename/file_rename.py
@@ -4,7 +4,8 @@ import os
 #function to rename files
 def rename_files():
     # get file names from folder
-    os.listdir("/home/phad/version-control/python-practice/file_rename")
+    file_list = os.listdir(r"/home/phad/version-control/python-practice/file_rename/prank")
+    print(file_list)
     # rename the files removing the numbers
 
 
