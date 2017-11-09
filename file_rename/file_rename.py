8de79d62b6774c3acd3c19de3091b64d16ad4a3b
Suhail Ahamed <suhail511@gmail.com>
2017-11-09 11:35:32 +0400

    get file name with os.listdir()

---
+++ file_rename/file_rename.py
@@ -0,0 +1,11 @@
+import os
+
+
+#function to rename files
+def rename_files():
+    # get file names from folder
+    os.listdir("/home/phad/version-control/python-practice/file_rename")
+    # rename the files removing the numbers
+
+
+rename_files()
