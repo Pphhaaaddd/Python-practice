ea723a73426d2a8c21eacddb9b8b500c2a8a7d83
Suhail Ahamed <suhail511@gmail.com>
2017-11-09 16:22:31 +0400

    add file to check for profinity. Read and check functions

---
+++ check_profanity/check_profanity.py
@@ -0,0 +1,20 @@
+import urllib
+
+#Read text from file
+def read_text():
+    texts = open("/home/phad/version-control/python-practice/check_profanity/texts.txt")
+    contents_of_file = texts.read()
+    #print(contents_of_file)
+    texts.close()
+    check_for_profanity(contents_of_file)
+###########################
+
+#check for curse words
+def check_for_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close()
+###########################
+
+read_text()
